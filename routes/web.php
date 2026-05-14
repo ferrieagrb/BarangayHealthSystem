c3d896fc550f25ef5578625011ea3619b6ee933f
ferrieagrb <blanzaferre@gmail.com>
2026-05-14 15:31:32 +0800

    RE-BUILD

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ use App\Http\Controllers\AdminLogController;
 use App\Http\Controllers\AdminSettings;
 use App\Http\Controllers\VehicleLogController;
 use App\Http\Controllers\AdminUserManagementController;
-
 /*
 |--------------------------------------------------------------------------
 | PUBLIC ROUTES
