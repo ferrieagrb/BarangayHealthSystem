e7727812a4ec4428062e8dfa6d865ebaf184189e
ferrieagrb <blanzaferre@gmail.com>
2026-05-14 10:15:24 +0800

    Add Citizen Views [PART 1] - NOT DONE \ ADDITIONAL

--- resources/views/citizenhome.blade.php
+++ resources/views/citizenhome.blade.php
@@ -20,9 +20,9 @@
         <img src="../images/amuyong.png" height="75px" width="75px">
         <h2>BARANGAY AMUYONG</h2>
         <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Calendar</a></li>
-            <li><a href="#">Announcements</a></li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/calendar">Calendar</a></li>
+            <li><a href="/announcements">Announcements</a></li>
             <li><a href="/login">Login</a></li>
         </ul>
     </div>
