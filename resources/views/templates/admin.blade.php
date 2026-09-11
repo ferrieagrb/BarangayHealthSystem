40c3ead0550dc16733273a305ebe21d2ab518bdd
ferrieagrb <blanzaferre@gmail.com>
2026-09-12 01:49:32 +0800

    Inventory

--- resources/views/templates/admin.blade.php
+++ resources/views/templates/admin.blade.php
@@ -45,28 +45,14 @@
             <li>
                 <a href="/admin/analytics">
                     <i class="bx bx-network-chart"></i>
-                    <span class="nav-item">Analytics</span>
+                    <span class="nav-item">Reports</span>
                 </a>
             </li>
 
             <li>
-                <a href="/admin/logs">
-                    <i class="bx bx-folder"></i>
-                    <span class="nav-item">System Logs</span>
-                </a>
-            </li>
-
-            <li>
-                <a href="/admin/settings">
-                    <i class="bx bx-cog"></i>
-                    <span class="nav-item">System Settings</span>
-                </a>
-            </li>
-
-            <li>
-                <a href="#">
-                    <i class="bx bx-shield"></i>
-                    <span class="nav-item">Security</span>
+                <a href="/supplies">
+                    <i class="bx bx-box"></i>
+                    <span class="nav-item">Supplies</span>
                 </a>
             </li>
 
